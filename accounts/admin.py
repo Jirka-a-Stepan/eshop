0e002014cd502758ea10497f90dd596418db5378
Jiří Trnečka <trnecka.jiri@gmail.com>
2022-10-28 12:41:22 +0200

    Created connection the custom authentication model to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name']
+
+
+admin.site.register(User, UserAdmin)
