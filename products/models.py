82cdfa0ea4ad94185a5f435ca1c362df0d2b3411
Kocur <kocuraq@gmail.comgit congig --global user.name Kocurgit config --global user.email kocuraq@gmail.com>
2022-10-30 11:18:24 +0100

    Product_name

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
 
 # Create your models here.
