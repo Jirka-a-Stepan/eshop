a7cbfff9756d32bb9c624e0437b598bede59d9fc
Jiří Trnečka <trnecka.jiri@gmail.com>
2022-11-05 14:19:33 +0100

    Added form for update profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,30 @@
 from django.contrib.auth import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
+from accounts.forms import ProfileForm
 from accounts.models import User
 
 
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', )
+        fields = ('email', 'first_name', 'last_name',)
 
 
 class RegistrationFormView(CreateView):
-    template_name = 'accounts/registration.html'
+    template_name = 'registration.html'
     form_class = RegistrationForm
     success_url = reverse_lazy('registration_form')
+
+
+class EditProfileView(UpdateView):
+    template_name = "edit_profile.html"
+    model = User
+    form_class = ProfileForm
+    success_url = reverse_lazy("products")
+
+
